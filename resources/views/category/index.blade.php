b7c3fa4167789300706087e526154fb010d23e25
mamemaru00 <k.shouhei0711@gmail.com>
2022-11-07 18:01:30 +0900

    index

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,6 +1,10 @@
-<h1>カテゴリー一覧画面</h1>
+@extends('layouts.app')
 
-@foreach ($categories as $category)
-    <p>{{ $category->category_id }}</p>
-    <p>{{ $category->category_name }}</p>
-@endforeach
+@section('content')
+  <h1>カテゴリー一覧画面</h1>
+
+  @foreach ($categories as $category)
+      <p>{{ $category->category_id }}</p>
+      <p>{{ $category->category_name }}</p>
+  @endforeach
+@endsection
--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -1,12 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
 <h1>もくもく会一覧画面</h1>
 
-@foreach($events as $event)
-    <p>{{ $event->event_id }}</p>
-    <p>{{ $event->category_id }}</p>
-    <p>{{ $event->title }}</p>
-    <p>{{ $event->date }}</p>
-    <p>{{ $event->start_time }}</p>
-    <p>{{ $event->end_time }}</p>
-    <p>{{ $event->content }}</p>
-    <p>{{ $event->entry_id }}</p>
-@endforeach    
+    @foreach($events as $event)
+        <p>{{ $event->event_id }}</p>
+        <p>{{ $event->category_id }}</p>
+        <p>{{ $event->title }}</p>
+        <p>{{ $event->date }}</p>
+        <p>{{ $event->start_time }}</p>
+        <p>{{ $event->end_time }}</p>
+        <p>{{ $event->content }}</p>
+        <p>{{ $event->entry_id }}</p>
+    @endforeach
+
+<button class="btn btn-primary">検索</button>
+@endsection
