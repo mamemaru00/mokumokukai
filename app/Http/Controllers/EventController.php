31ffd1253103496cb3772fdcd15d79d2d9ddcdd7
mamemaru00 <k.shouhei0711@gmail.com>
2022-11-07 16:34:48 +0900

    コントローラ　ファット状態

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class EventController extends Controller
 {
+    public function __construct()
+    {
+        $this->event = new Event();        
+    }
+    
     public function index()
     {
-        return view('event.index');
+        // Eloquentでeventsテーブルにあるデータを全て取得
+        $events = Event::get();
+
+        //　実務のレベルになると、記述量も増えて同じような記述をコントローラーに書くのはよくない
+        $events = Event::select('event_id', 'title')
+                                ->leftJoin('')
+                                ->where('category_id', 1)
+                                ->orderBy('category_id', 'ASC')
+                                ->get();
+
+        return view('event.index', compact('events'));
     }
 }
