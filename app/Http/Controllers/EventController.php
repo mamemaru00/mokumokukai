ac072de6e6ba5298e1c0d95a47b1e9a2b6ba401a
mamemaru00 <k.shouhei0711@gmail.com>
2022-11-10 18:23:54 +0900

    検索欄 修正

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -18,21 +18,21 @@ class EventController extends Controller
 
     public function index(Request $request)
     {
-        // 検索したキーワード
-        $word = $request->search;
+         // 検索したキーワード
+         $word = $request->search;
 
-        // 検索フォームに文字が入力されているか判定
-        if (!is_null($word)) {
-            // $wordの値がある→nullではない→検索フォームに何かしら入力されている
-            // キーワードをもとに、部分一致するイベントを取得
-            $events = $this->event->allEventData($word);
-        } else {
-            // $wordの値がない→null→検索フォームに何も入力されていない、つまり初期状態
-            // Eloquentでeventsテーブルにあるデータを全て取得
-            $events = $this->event->allEventData();
-        }
-
-        return view('event.index', compact('events'));
+         // 検索フォームに文字が入力されているか判定
+         if (!is_null($word)) {
+             // $wordの値がある→nullではない→検索フォームに何かしら入力されている
+             // キーワードをもとに、部分一致するイベントを取得
+             $events = $this->event->searchWord($word);
+         } else {
+             // $wordの値がない→null→検索フォームに何も入力されていない、つまり初期状態
+             // Eloquentでeventsテーブルにあるデータを全て取得
+             $events = $this->event->allEventsData();
+         }
+ 
+         return view('event.index', compact('events'));
     }
 
     /**
